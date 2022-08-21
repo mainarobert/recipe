d363befd5a0e602ef1ae8264b2afd86e723bbc10
RobertMaina <robertmaina254@gmail.com>
2022-08-21 23:39:23 +0300

    staticfiles and media whitenoise

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -136,6 +136,7 @@ STATICFILES = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
