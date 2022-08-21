6624ada772383173372d1659ead309927e19aa1e
RobertMaina <robertmaina254@gmail.com>
2022-08-21 23:07:56 +0300

    staticfiles connfig

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -138,8 +138,8 @@ STATICFILES = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/staticfiles/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media/')
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 os.makedirs(MEDIA_ROOT, exist_ok=True)
